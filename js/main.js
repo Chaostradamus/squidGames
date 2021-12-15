e31e8356b12b252f1549b77571f4c0201eeae92c
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-12-15 03:02:17 -0500

    first drop

---
+++ js/main.js
@@ -0,0 +1,78 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+renderer.setClearColor(0xb7c3f3, 1);
+
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+//globals
+const start_position = 3;
+const start_pos = -start_position;
+
+function createCube(size, positionX) {
+  const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  cube.position.x = positionX;
+  scene.add(cube);
+}
+
+camera.position.z = 5;
+
+const loader = new THREE.GLTFLoader();
+
+class Doll {
+  constructor() {
+    loader.load("../models/scene.gltf", (gltf) => {
+      scene.add(gltf.scene);
+      gltf.scene.scale.set(0.4, 0.4, 0.4);
+      gltf.scene.position.set(0, -1, 0);
+      this.doll = gltf.scene;
+    });
+  }
+  lookBackward() {
+    // this.doll.rotation.y = -3.15;
+    gsap.to(this.doll.rotation, { y: -3.15, duration: 0.45 });
+  }
+  lookFoward() {
+    // this.doll.rotation.y = 0;
+    gsap.to(this.doll.rotation, { y: 0, duration: 0.45 });
+  }
+}
+
+function createTrack() {
+  createCube({ w: 0.2, h: 1.5, d: 1 }, -3);
+}
+
+createTrack();
+
+let doll = new Doll();
+setTimeout(() => {
+  doll.lookBackward();
+}, 1000);
+
+function animate() {
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
+}
+animate();
+
+window.addEventListener("resize", onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
